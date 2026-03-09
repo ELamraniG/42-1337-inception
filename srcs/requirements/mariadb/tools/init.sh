32558ba18b04495792ece3cc9a4581a052d41c46
ELamraniG <moha.mati@hotmail.com>
2026-03-09 22:34:44 +0000

    added some comments

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -11,19 +11,18 @@ while ! mysqladmin ping --silent 2>/dev/null; do
     sleep 1
 done
 
+
+#test if we really need this FLUSH PRIVILEGES;
 echo "Creating user and database..."
 mysql -u root <<lopo
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-FLUSH PRIVILEGES;
 lopo
 
 mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
 wait $MYSQLD_PID
-
-echo "Init done, starting MariaDB..."
 
 exec mysqld_safe
